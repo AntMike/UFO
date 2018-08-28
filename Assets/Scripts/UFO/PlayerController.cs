9addb31d312be8b45ad2f801c59c8b61d30352a3
MrSherlok <mikeAntoniv@gmail.com>
2018-08-28 14:35:56 +0300

    Particles, new moving system, Extended moving and refactoring

--- Assets/Scripts/UFO/PlayerController.cs
+++ Assets/Scripts/UFO/PlayerController.cs
@@ -2,108 +2,221 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-    public float speed;
-    public float turnspeed;
-    public Transform child;
-    public float minAngle;
-    public float maxAngle;
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Components")]
 
-    public float maxVerticalSpeed = 25;
-    public float maxHorizontalSpeed = 15;
-    Rigidbody rbody;
+    public List<ParticleSystem> UFOParticle;
+    public Transform UFOChild;
+    public Transform UFOModel;
+    private Transform UFOTrans;
+    private Rigidbody UFORgbd;
 
-    private Transform playerTransform;
-    private Vector3 relative;
+    //angle to tangage UFO
+    private float _minTangageAngle = -15;
+    private float _maxTangageAngle = 15;
+    //time before UFO starts faling down
+    private float _timeBeforeFalling = 3;
 
+    [Header("Move parametrs")]
+    public float MoveAcceleration;
+    public float MaxVerticalSpeed = 25;
+    public float MaxHorizontalSpeed = 15;
+    private Vector2 _moveForce;
 
-    // Use this for initialization
-    void Start()
+    [Space]
+    public float MouseSensitivity = 100;
+    private float _mouseSensativeX = 0.0f;
+    private float _mouseSensativeY = 0.0f;
+
+   
+    private void Start()
     {
-        rbody = GetComponent<Rigidbody>();
-        playerTransform = transform;
+        //disable cursor in play mode
+        Cursor.visible = false;
+        //set rigidbody abd transform
+        UFORgbd = GetComponent<Rigidbody>();
+        UFOTrans = transform;
     }
 
-    Vector3 nextVerticalVelocity;
-    Vector3 nextHorizontalVelocity;
-    void FixedUpdate()
+    private void FixedUpdate()
     {
+        //check if we need to falling down
+        WaitForIdle();
 
-        child.Rotate(0, 2 + Mathf.Abs(rbody.velocity.z) / 20 + Mathf.Abs(rbody.velocity.x) / 20, 0);
-
+        //calculate force to move
         MoveVertical();
         MoveHorizontal();
-        RotateUFO();
+        //calculate rotating
+        RotateUFOInMove();
+        RotateUFOAroundHimself();
+        //camera rotating
+        MovePlayerToMouse();
+        //activating particles
+        ParticleControll();
 
-        Debug.Log(rbody.velocity);
+        //player moves
+        transform.Translate(new Vector3(_moveForce.x * Time.deltaTime, 0, _moveForce.y*Time.deltaTime));
 
     }
 
+    /// <summary>
+    /// Calculate mouse position and rotate player to mouse
+    /// </summary>
+    private void MovePlayerToMouse()
+    {
+
+        _mouseSensativeX += Input.GetAxis("Mouse X") * MouseSensitivity * 0.02f;
+        _mouseSensativeY -= Input.GetAxis("Mouse Y") * MouseSensitivity * 0.02f;
+
+        _mouseSensativeY = ClampAngle(_mouseSensativeY);
+
+        // Set rotation
+        Quaternion rotation = Quaternion.Euler(_mouseSensativeY, _mouseSensativeX, 0);
+        UFOTrans.rotation = rotation;
+    }
+
+    /// <summary>
+    /// Keep euler angle between -360 and 360 degree
+    /// </summary>
+    /// <param name="angle"> Angle that we need to check and fix</param>
+    /// <returns>That angle with fix</returns>
+    private float ClampAngle(float angle)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+
+        return Mathf.Clamp(angle, -60, 80);
+    }
+    
+    /// <summary>
+    /// Calculate vertical movement force
+    /// </summary>
     private void MoveVertical()
     {
-        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.001f)
+        if (Mathf.Abs(_moveForce.y) < MaxVerticalSpeed)
         {
-            nextVerticalVelocity = rbody.velocity + (Vector3.forward * turnspeed * Input.GetAxis("Vertical") * Time.deltaTime);
-            if (Mathf.Abs(nextVerticalVelocity.z) < maxVerticalSpeed)
-                rbody.velocity = nextVerticalVelocity;
-        }
-        else
-        {
-            if (rbody.velocity.z < -1)
+            if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.01f)
             {
-                rbody.velocity += Vector3.forward * speed / 2 * Time.deltaTime;
+                if (Mathf.Abs(_moveForce.y + Input.GetAxis("Vertical")) < MaxVerticalSpeed)
+                    _moveForce.y += Input.GetAxis("Vertical");
             }
             else
             {
-                if (rbody.velocity.z > 1)
+                if (_moveForce.y > 1)
                 {
-                    rbody.velocity += Vector3.back * speed / 2 * Time.deltaTime;
+                    _moveForce.y -= MoveAcceleration / 2 * Time.deltaTime;
+                }
+                else if (_moveForce.y < -1)
+                {
+                    _moveForce.y += MoveAcceleration / 2 * Time.deltaTime;
                 }
                 else
                 {
-                    rbody.velocity = new Vector3(rbody.velocity.x, rbody.velocity.y, 0);
+                    _moveForce.y = 0;
                 }
             }
         }
 
     }
 
+    /// <summary>
+    /// Calculate horizontal movement force
+    /// </summary>
     private void MoveHorizontal()
     {
-        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.001f)
+
+        if (Mathf.Abs(_moveForce.x) < MaxHorizontalSpeed)
         {
-            nextHorizontalVelocity = rbody.velocity + (Vector3.right * turnspeed * Input.GetAxis("Horizontal") * Time.deltaTime);
-            if (Mathf.Abs(nextHorizontalVelocity.x) < maxHorizontalSpeed)
-                rbody.velocity = nextHorizontalVelocity;
-        }
-        else
-        {
-            if (rbody.velocity.x < -1)
+            if (Input.GetAxis("Horizontal") != 0)
             {
-                rbody.velocity += Vector3.right * turnspeed / 2 * Time.deltaTime;
+
+                if (Mathf.Abs(_moveForce.x + Input.GetAxis("Horizontal")) < MaxHorizontalSpeed)
+                    _moveForce.x += Input.GetAxis("Horizontal");
             }
             else
             {
-                if (rbody.velocity.x > 1)
+                if (_moveForce.x > 1)
                 {
-                    rbody.velocity += Vector3.left * turnspeed / 2 * Time.deltaTime;
+                    _moveForce.x -= MoveAcceleration / 2 * Time.deltaTime;
+                }
+                else if (_moveForce.x < -1)
+                {
+                    _moveForce.x += MoveAcceleration / 2 * Time.deltaTime;
                 }
                 else
                 {
-                    rbody.velocity = new Vector3(0, rbody.velocity.y, rbody.velocity.z);
+                    _moveForce.x = 0;
                 }
             }
         }
     }
 
-    private void RotateUFO()
+    /// <summary>
+    /// Check idle time to falling down the UFO
+    /// </summary>
+    private void WaitForIdle()
+    {
+        if (!Input.anyKey)
+        {
+            _timeBeforeFalling -= Time.deltaTime;
+            if (_timeBeforeFalling <= 0)
+            {
+                UFORgbd.useGravity = true;
+            }
+        }
+        else
+        {
+            if (_timeBeforeFalling != 3)
+            {
+                _timeBeforeFalling = 3;
+                UFORgbd.useGravity = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Calculate tangage
+    /// </summary>
+    private void RotateUFOInMove()
+    {
+        float angH = Mathf.Lerp(_minTangageAngle, _maxTangageAngle, Mathf.InverseLerp(-MaxHorizontalSpeed, MaxHorizontalSpeed, _moveForce.x));
+        float angV = Mathf.Lerp(_minTangageAngle, _maxTangageAngle, Mathf.InverseLerp(-MaxVerticalSpeed, MaxVerticalSpeed, _moveForce.y));
+        UFOChild.localEulerAngles = new Vector3(angV, 0, -angH);
+    }
+
+    /// <summary>
+    /// Rotating UFO around himself in move and idle
+    /// </summary>
+    private void RotateUFOAroundHimself()
     {
 
-            float angH = Mathf.Lerp(minAngle, maxAngle, Mathf.InverseLerp(-maxHorizontalSpeed, maxHorizontalSpeed, rbody.velocity.x));
-        float angV = Mathf.Lerp(minAngle, maxAngle, Mathf.InverseLerp(-maxVerticalSpeed, maxVerticalSpeed, rbody.velocity.z));
-                playerTransform.eulerAngles = new Vector3(angV, 0, -angH);
-
-
-
+        UFOModel.Rotate(0, 2 + Mathf.Abs(_moveForce.y) / 20 + Mathf.Abs(_moveForce.x) / 20, 0);
+    }
+    
+    /// <summary>
+    /// Set particle speed to UFO speed and turn off then in idle
+    /// </summary>
+    private void ParticleControll()
+    {
+        foreach (ParticleSystem _particle in UFOParticle)
+        {
+            if (_moveForce.y > 0)
+                _particle.startSpeed = _moveForce.y * 2;
+            if (_moveForce.y <= 0)
+            {
+                _particle.Clear();
+                _particle.Pause();
+            }
+            else
+            {
+                if (_moveForce.y > 0)
+                {
+                    _particle.Play();
+                }
+            }
+        }
     }
 }
